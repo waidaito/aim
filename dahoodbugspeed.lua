f357d967b1aa217f80d948c9416d6f993db9ad33
waidaito <scriptcode699@gmail.com>
2026-03-03 22:54:14 +0700

    dahoodbugspeed.lua

--- dahoodbugspeed.lua
+++ dahoodbugspeed.lua
@@ -2,18 +2,16 @@ if _G.SpeedAnimRunning then return end
 _G.SpeedAnimRunning = true
 
 local lp = game:GetService("Players").LocalPlayer
+local rs = game:GetService("RunService")
 local TargetAnimID = "rbxassetid://3152394906"
 local SpeedAmount = 200
 
-game:GetService("RunService").Heartbeat:Connect(function()
+rs.Stepped:Connect(function()
     local char = lp.Character
     local hum = char and char:FindFirstChildOfClass("Humanoid")
     local hrp = char and char:FindFirstChild("HumanoidRootPart")
-    local cam = workspace.CurrentCamera
 
-    if hum and hrp then
-        if hum.Sit then hum.Sit = false end
-
+    if hum and hrp and not hum.Sit then
         local isPlaying = false
         for _, track in ipairs(hum:GetPlayingAnimationTracks()) do
             if track.Animation.AnimationId == TargetAnimID then
@@ -24,7 +22,7 @@ game:GetService("RunService").Heartbeat:Connect(function()
 
         if isPlaying and hum.MoveDirection.Magnitude > 0 then
             hrp.Velocity = Vector3.new(hum.MoveDirection.X * SpeedAmount, hrp.Velocity.Y, hum.MoveDirection.Z * SpeedAmount)
-            cam.CameraSubject = hum
+            hrp.RotVelocity = Vector3.new(0, 0, 0)
         end
     end
 end)
